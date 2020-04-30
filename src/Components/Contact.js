c084c20548b620a1bdff4bc6a9246bccb37516b2
sticklercarla <carlastickler@gmail.com>
2020-04-29 22:36:35 -0400

    contact page

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,10 +1,25 @@
 import React from "react";
+import ReactContactForm from 'react-mail-form';
 
 const Contact = () => {
 
     return (
         <div className='contact'>
-            HELLO From the contact
+            <div className="left-panel">
+                CONTACT
+            </div>
+            <div className="rt-panel">
+                <div>
+                    <ReactContactForm className="email" contentsPlaceholder="Like my work? Want to collaborate? Just wanna say hi? Let's see what ya got!" contentsMaxLength="600" titleMaxLength="30"  to="carlastickler@gmail.com" buttonText="Email me!" />
+                </div>
+                <div>
+                    <a href="https://github.com/sticklercarla" target="_blank" rel="noopener noreferrer">Github</a>
+                    <a href="https://medium.com/from-the-stage-to-the-screen" target="_blank" rel="noopener noreferrer">Medium</a>
+                    <a href="https://www.linkedin.com/in/carlastickler/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+                    <a href="https://twitter.com/sticklercarla" target="_blank" rel="noopener noreferrer">Twitter</a>
+                    <a href="https://www.instagram.com/sticklercarla/" target="_blank" rel="noopener noreferrer">Instagram</a>
+                </div>
+            </div>
         </div>
     )
 }
--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -15,22 +15,34 @@ class Portfolio extends React.Component {
      
         return (
             <div className="portfolio-div">
-                <div className="port-side-bar">
+                <div className="left-panel">
                     PORTFOLIO
                 </div>
                 <div className="video-div">
                     <figure className="video">
                         <YouTube className="vid" videoId="FI3w12LSDto" opts={opts} onReady={this._onReady}/>
                         <figcaption>Are you and actor who needs to keep track of your audition stats? Who was in the room? How did it go? How many callbacks do you get vs how many shows did you book? This app makes visualizing your data easy and fun!</figcaption>
-                        
+                        <div>
+                            <a href="https://github.com/sticklercarla/broadway-life-front-end" target="_blank" rel="noopener noreferrer">Github Frontend</a>
+                            <a href="https://github.com/sticklercarla/broadway-life-api" target="_blank" rel="noopener noreferrer">Github Backend</a>
+                            <a href="https://broadway-audition-life-app.herokuapp.com/" target="_blank" rel="noopener noreferrer">View Live</a>
+                        </div>
                     </figure>
                     <figure className="video">
                         <YouTube className="vid" videoId="QKzxsU_6vuE" opts={opts} onReady={this._onReady} />
                         <figcaption>Match your favorite Elphabas and keep track of your high scores!</figcaption>
+                        <div>
+                            <a href="https://github.com/sticklercarla/Wicked-Match-Game" target="_blank" rel="noopener noreferrer">Github Frontend</a>
+                            <a href="https://github.com/sticklercarla/Wicked-Match-Game-API" target="_blank" rel="noopener noreferrer">Github Backend</a>
+                            <a href="https://wicked-match-game.herokuapp.com/" target="_blank" rel="noopener noreferrer">View Live</a>
+                        </div>
                     </figure>
                     <figure className="video">
                         <YouTube className="vid" videoId="9wNs7G9jcLg" opts={opts} onReady={this._onReady} />
                         <figcaption>Send your friends emojis and receive karma points, because giving makes you feel good!</figcaption>
+                        <div>
+                        <a href="https://github.com/sticklercarla/Karma" target="_blank" rel="noopener noreferrer">Github</a>
+                        </div>
                     </figure>
                 </div>
             </div>
